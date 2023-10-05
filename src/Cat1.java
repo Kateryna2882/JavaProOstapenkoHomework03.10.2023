f17f74ac9fc19b040c20db3d6d2123750a6db95b
Kateryna2882 <111122172+Kateryna2882@users.noreply.github.com>
2023-10-05 18:25:30 +0300

    Add fields Cat1 class

---
+++ src/Cat1.java
@@ -0,0 +1,21 @@
+public class Cat1 extends Animal {
+
+    public static int count;
+
+    public void Swim(String name) {
+
+        System.out.println("Cat " + name + " can't swim");
+    }
+
+    public void Run(String name, int action) {
+
+        System.out.println("Cat" + name + action);
+    }
+
+    public static int getCountCat() {
+
+        return count;
+    }
+}
+
+
